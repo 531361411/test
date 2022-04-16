f055c8b51b60048e6f9c2a6177cca4e94479d04c
yangwei <yang.wei@5th.work>
2022-04-16 20:02:03 +0800

    修改路径-1

--- artifact2image.sh
+++ artifact2image.sh
@@ -3,17 +3,17 @@
 set -eu #如有报错或取不到变量情况停止执行
 
 #声名常量
-IMG_REGISTRY="192.168.87.129:5000" #镜像仓库
+IMG_REGISTRY="172.28.236.82:5000" #镜像仓库
 IMG_NAME="cicd-demo"
 IMG_TAG=`date "+%Y%M%d_%H%M"`      #镜像标签，如 20201223_1351
 IMG_FULL_NAME="${IMG_REGISTRY}/${IMG_NAME}:${IMG_TAG}" #镜像上传与拉取的名称
-
+echo 'build-----'
 #构建镜像
 docker build -t ${IMG_FULL_NAME} .
-
+echo 'push-----'
 #推送镜像
 docker push ${IMG_FULL_NAME}
-
+echo 'remove-----'
 #删除本地镜像
 docker rmi ${IMG_FULL_NAME}
 
--- deploy2k8s.sh
+++ deploy2k8s.sh
@@ -5,19 +5,19 @@ set -eu
 
 #定义常量
 PROJECT_NAME="cicd-demo"
-UPLOAD_DIR="/home/hellxz/apps/${PROJECT_NAME}"
+UPLOAD_DIR="/home/root/apps/${PROJECT_NAME}"
 FILE_NAME="${UPLOAD_DIR}/deploy.yaml"
-SSH_USER="hellxz"
-SSH_IP="192.168.87.129"
-
+SSH_USER="root"
+SSH_IP="172.28.236.82"
+echo 'start---'
 #首先删除待上传目录的同名文件
 ssh ${SSH_USER}@${SSH_IP} "rm -rf ${FILE_NAME}"
-
+echo 'second---'
 #确保部署文件目录存在
 ssh ${SSH_USER}@${SSH_IP} "mkdir -p ${UPLOAD_DIR}"
-
+echo 'last---'
 #远程复制部署文件
 scp -r deploy.yaml ${SSH_USER}@${SSH_IP}:${FILE_NAME}
-
+echo 'end---'
 #远程执行部署命令
 ssh ${SSH_USER}@${SSH_IP} "kubectl apply -f ${FILE_NAME}"

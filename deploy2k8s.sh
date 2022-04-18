1d6b28d731b5615b438f54d2fdd12b000bb61e6d
yangwei <yang.wei@5th.work>
2022-04-18 10:09:37 +0800

    修改路径-2

--- deploy2k8s.sh
+++ deploy2k8s.sh
@@ -11,13 +11,13 @@ SSH_USER="root"
 SSH_IP="172.28.236.82"
 echo 'start---'
 #首先删除待上传目录的同名文件
-ssh-keygen -R ${SSH_USER}@${SSH_IP} "rm -rf ${FILE_NAME}"
+ssh ${SSH_USER}@${SSH_IP} "rm -rf ${FILE_NAME}"
 echo 'second---'
 #确保部署文件目录存在
-ssh-keygen -R ${SSH_USER}@${SSH_IP} "mkdir -p ${UPLOAD_DIR}"
+ssh ${SSH_USER}@${SSH_IP} "mkdir -p ${UPLOAD_DIR}"
 echo 'last---'
 #远程复制部署文件
 scp -r deploy.yaml ${SSH_USER}@${SSH_IP}:${FILE_NAME}
 echo 'end---'
 #远程执行部署命令
-ssh-keygen -R ${SSH_USER}@${SSH_IP} "kubectl apply -f ${FILE_NAME}"
+ssh ${SSH_USER}@${SSH_IP} "kubectl apply -f ${FILE_NAME}"
